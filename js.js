2e49ae45152f17953cf0d035612664f9b198973b
Leonardo <leonardocian1999@gmail.com>
2023-06-29 16:17:44 -0300

    a

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -22,38 +23,44 @@ function showSlides(n) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += "active";
+  /* dots[slideIndex-1].className += "active"; */
 }
   
 
- var integrante=[];{
- const int1 = {nombre:"Asa Phelps",apodo:"Asa",rango:"Soldado de asalto",estado:"Muerto, año 2000"}
- const int2 = {nombre:"Charles Montgomery Burns",apodo:"Burns",rango:"Soldado raso",estado:"Vivo"}
- const int3 = {nombre:"Arnie Gómez",apodo:"Gómez",rango:"Soldado raso - Conductor de tanque",estado:"Muerto, año 1979"}
- const int4 = {nombre:"Iggy Gorgory",apodo:"Gorgory",rango:"Soldado Granadero",estado:"Muerto, año 1979"}
- const int5 = {nombre:"Etch Westgrin",apodo:"Momo",rango:"Soldado raso",estado:"Muerto, año 1979"}
- const int6 = {nombre:"Griff McDonald",apodo:"Pepe",rango:"Soldado de asalto",estado:"Muerto, año 1979"}
- const int7 = {nombre:"Abraham Simpson",apodo:"Simpson",rango:"Sargento",estado:"Vivo"}
- const int8 = {nombre:"Sheldon Skinner",apodo:"Skinner",rango:"Radio-Operador",estado:"Muerto, año 1979"}
- const int9 = {nombre:"Milton Haas",apodo:"Sote",rango:"Soldado de asalto",estado:"Muerto, año 1945"}
- }
+ var integrante=[
+  {nombre:"Asa Phelps",apodo:"Asa",rango:"Soldado de asalto",estado:"Muerto, año 2000"},
+{nombre:"Charles Montgomery Burns",apodo:"Burns",rango:"Soldado raso",estado:"Vivo"},
+{nombre:"Arnie Gómez",apodo:"Gómez",rango:"Soldado raso - Conductor de tanque",estado:"Muerto, año 1979"},
+{nombre:"Iggy Gorgory",apodo:"Gorgory",rango:"Soldado Granadero",estado:"Muerto, año 1979"},
+{nombre:"Etch Westgrin",apodo:"Momo",rango:"Soldado raso",estado:"Muerto, año 1979"},
+{nombre:"Griff McDonald",apodo:"Pepe",rango:"Soldado de asalto",estado:"Muerto, año 1979"},
+{nombre:"Abraham Simpson",apodo:"Simpson",rango:"Sargento",estado:"Vivo"},
+{nombre:"Sheldon Skinner",apodo:"Skinner",rango:"Radio-Operador",estado:"Muerto, año 1979"},
+{nombre:"Milton Haas",apodo:"Sote",rango:"Soldado de asalto",estado:"Muerto, año 1945"}
+ ];
  
- let prev = document.querySelector("prev");
- const next = document.querySelector("next");
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
 
   function showNext() {
-    var objeto = integrante[slideIndex];
-    var info = document.createElement("info");
-    info.textContent= "Nombre:"+objeto.nombre;
-    var contenedor = document.querySelector(".Informacion");
+    let objeto = integrante;
+    let contenedor = document.querySelector(".Informacion");
+    let info = document.getElementById('info');
+    //let cadena ="Nombre:"+objeto.nombre+"\nApodo: "+objeto.apodo+"\nRango: "+objeto.rango+"\nEstado: "+objeto.estado;
+const mapArray =() => {objeto.map(({key, value}) =>({[key]: value}))};
+    info.textContent=mapArray();
     contenedor.appendChild(info);
-  }; 
+  };
+
   function showPrev(){
 
   };
   next.addEventListener('click', ()=>{
-    console.log("Click");
+    showNext();
   })
+  prev.addEventListener('click', ()=>{
+showPrev();
+  });
 
   
   
